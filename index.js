fbd931166e555d8a393f4069c88d9cc2233fa229
ayush_sinha <you@example.com>
2025-03-02 14:29:12 +0530

    added header

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from "express"
+
 const app= express();
 const port= 3000;
+
+app.use(express.static("public"));
+
+
 app.get("/",(req,res)=>{
-    res.send("hii");
+    res.render("home.ejs");
+})
+app.get("/new_blog",(req,res)=>{
+    res.render("new_blog.ejs");
+})
+app.get("/view_other",(req,res)=>{
+    res.render("view_others.ejs");
 })
 app.listen(port,()=>{
-    console.log("listening0");
+    console.log("listening");
 })
